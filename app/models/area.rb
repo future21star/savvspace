53914f75adfadc19ede6d66ad8f1a8a7867584ed
Aaron Campos <frater.arc@gmail.com>
2016-11-06 22:01:46 -0800

    Add SimpleForm

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,7 @@
 class Area < ActiveRecord::Base
   has_one :ambassadorship, dependent: :destroy
   has_many :articles, dependent: :nullify
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
