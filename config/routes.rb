2826a28dfb090c87c69dae4200a69f1e842261d1
Aaron Campos <frater.arc@gmail.com>
2017-01-26 11:36:17 -0800

    Update navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get '/dashboard' => 'welcome#dashboard', as: :dashboard
+  get '/about' => 'welcome#about', as: :about
+  get '/contact' => 'welcome#contact', as: :contact
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
