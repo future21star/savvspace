8588b26cba774523e700c81bee1996a19ecb0851
Aaron Campos <frater.arc@gmail.com>
2017-02-17 22:47:20 -0800

    Display login errors

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -10,4 +10,10 @@ module BootstrapHelper
   def tab_link(text, tab_name)
     link_to(text, "##{tab_name}", role: "tab", data: {toggle: "tab"}, aria: {controls: "tab_name"})
   end
+
+  def glyphicon(icon_name, text=nil)
+    capture_haml do
+      haml_tag("span.glyphicon.glyphicon-#{icon_name}", nil, {aria: {hidden: true} })
+    end
+  end
 end
