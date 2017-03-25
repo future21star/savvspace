94db7bac48248851685a1becbda5d36c3a2952c3
Aaron Campos <acampos@dhamira.com>
2017-03-24 21:12:20 -0700

    Use verified phone number as FROM number.

--- app/jobs/click_to_call_job.rb
+++ app/jobs/click_to_call_job.rb
@@ -3,8 +3,8 @@ class ClickToCallJob < ActiveJob::Base
 
   def perform(phone_call)
     client = Twilio::REST::Client.new
-    placed_call = client.calls.create(from: phone_call.from_phone.number,
-                                      to: phone_call.profile.phone.number,
+    placed_call = client.calls.create(from: "+18052090165",
+                                      to: phone_call.from_phone.number,
                                       applicationSid: ENV.fetch("CLICK_TO_CALL_SID"))
     phone_call.update(sid: placed_call.sid)
 
