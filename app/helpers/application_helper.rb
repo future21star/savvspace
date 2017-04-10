cc872e3b176a0f7aa12d014750f0003683ba772b
Aaron Campos <acampos@dhamira.com>
2017-04-09 17:41:16 -0700

    Set a max width for profile photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def background_image_url(image)
     return {} unless image
 
-    { style: "background-image: url(#{image.fullpath});" }
+    { style: "background-image: url(#{cl_image_path(image.path, width: 1200, crop: :lfill, quality: :auto)});" }
   end
 
   def rating_options(rateable)
