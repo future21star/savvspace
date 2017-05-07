659d2bf6942308673b3efb8c681a8b90d6f02b0f
Keti <ketevanzazarashvili.kz@gmail.com>
2017-05-07 14:34:31 +0400

    Modify Open House single property mailer

--- app/controllers/open_house_mailers_controller.rb
+++ app/controllers/open_house_mailers_controller.rb
@@ -1,22 +1,22 @@
-class PropertyMailersController < ApplicationController
+class OpenHouseMailersController < ApplicationController
   before_action :authenticate_user!
 
   def new
     @referrer_id = params[:referrer_id]
-    @property_id = params[:property_id]
+    @open_house_id = params[:open_house_id]
   end
 
   def create
     @referrer_id = params[:referrer_id]
-    item_id = params[:property_id]
+    item_id = params[:open_house_id]
 
-    SingleItemMailer.send_item_request(current_user.id, @referrer_id, recepients, item_id, 'Property')
+    SingleItemMailer.send_item_request(current_user.id, @referrer_id, recepients, item_id, 'OpenHouse')
   end
 
   private
 
   def recepients
-    mailer_params = params.require(:property_mailer).permit(:recepients, :include_sender)
+    mailer_params = params.require(:open_house_mailer).permit(:recepients, :include_sender)
 
     recepients = mailer_params[:recepients].split(',')
     recepients << current_user.email if mailer_params[:include_sender]
--- app/controllers/open_houses_controller.rb
+++ app/controllers/open_houses_controller.rb
@@ -2,13 +2,14 @@ class OpenHousesController < ApplicationController
   before_action :authenticate_user!
 
   def new_property_email
-    @property = OpenHouse.find(params[:open_house_id])
     @referrer_id = params[:referrer_id]
+    @property = OpenHouse.find(params[:open_house_id])
   end
 
   def send_property_email
-    @property = OpenHouse.find(params[:open_house_id])
     @referrer = Profile.find_by(id: params[:referrer_id])
+    @property = OpenHouse.find(params[:open_house_id])
+
     OpenHouseMailer.single_property(current_user, @property, @referrer).deliver_later
   end
 
