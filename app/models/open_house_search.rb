4e0ffa72344ba371860c1114b3806cab579a9016
Keti <ketevanzazarashvili.kz@gmail.com>
2017-05-17 21:56:17 +0400

    Add Searchable module and include it in Search models

--- app/models/open_house_search.rb
+++ app/models/open_house_search.rb
@@ -1,13 +1,5 @@
 class OpenHouseSearch < ActiveRecord::Base
-  belongs_to :mls_server
-  belongs_to :profile
-  validates :mls_server, presence: true
-
-  attr_accessor :limit, :offset
-
-  def results
-    query.order(order).limit(limit).offset(offset)
-  end
+  include Searchable
 
   def query
     scope = mls_server.open_houses.upcoming
@@ -16,8 +8,8 @@ class OpenHouseSearch < ActiveRecord::Base
       from_dates = from_date.reject(&:blank?)
 
       query_pattern = from_dates.inject([]) do |array, _date|
-        array << "starts_at BETWEEN ? AND ?"
-      end.join(" OR ")
+        array << 'starts_at BETWEEN ? AND ?'
+      end.join(' OR ')
 
       date_query = from_dates.inject([query_pattern]) do |array, date|
         time = Time.parse(date)
@@ -27,39 +19,20 @@ class OpenHouseSearch < ActiveRecord::Base
       scope = scope.where(date_query)
     end
 
-    scope = scope.where(["list_price >= ?", min_price]) if min_price.present?
-    scope = scope.where(["list_price <= ?", max_price]) if max_price.present?
+    scope = scope.where(['list_price >= ?', min_price]) if min_price.present?
+    scope = scope.where(['list_price <= ?', max_price]) if max_price.present?
     scope
   end
 
   def order
     case sort_by
     when PropertySearch::SORT_PRICE_LOW_TO_HIGH
-      "list_price asc"
+      'list_price asc'
     when PropertySearch::SORT_PRICE_HIGH_TO_LOW
-      "list_price desc"
+      'list_price desc'
     else
-      "starts_at asc"
+      'starts_at asc'
     end
   end
 
-  def total_results
-    query.count
-  end
-
-  def next_page_offset
-    offset.to_i + limit.to_i
-  end
-
-  def has_more?
-    next_page_offset < total_results
-  end
-
-  def limit
-    @limit ||= 12
-  end
-
-  def offset
-    @offset ||= 0
-  end
 end
--- app/models/property_search.rb
+++ app/models/property_search.rb
@@ -1,24 +1,16 @@
 class PropertySearch < ActiveRecord::Base
+  include Searchable
+
   SORT_NEWEST = 0
   SORT_PRICE_LOW_TO_HIGH = 1
   SORT_PRICE_HIGH_TO_LOW = 2
 
-  belongs_to :mls_server
-  belongs_to :profile
-  validates :mls_server, presence: true
-
-  attr_accessor :limit, :offset
-
-  def results
-    query.order(order).limit(limit).offset(offset)
-  end
-
   def query
     scope = mls_server.properties
-    scope = scope.where(["list_price >= ?", min_price]) if min_price.present?
-    scope = scope.where(["list_price <= ?", max_price]) if max_price.present?
-    scope = scope.where(["bedrooms >= ?", min_beds]) if min_beds.present?
-    scope = scope.where(["bedrooms <= ?", max_beds]) if max_beds.present?
+    scope = scope.where(['list_price >= ?', min_price]) if min_price.present?
+    scope = scope.where(['list_price <= ?', max_price]) if max_price.present?
+    scope = scope.where(['bedrooms >= ?', min_beds]) if min_beds.present?
+    scope = scope.where(['bedrooms <= ?', max_beds]) if max_beds.present?
     scope = scope.where(property_type: property_type) if property_type.present?
     scope = scope.where(area: area) if area.present?
     scope
@@ -27,31 +19,12 @@ class PropertySearch < ActiveRecord::Base
   def order
     case sort_by
     when PropertySearch::SORT_PRICE_LOW_TO_HIGH
-      "list_price asc"
+      'list_price asc'
     when PropertySearch::SORT_PRICE_HIGH_TO_LOW
-      "list_price desc"
+      'list_price desc'
     else
-      "mls_updated_at desc"
+      'mls_updated_at desc'
     end
   end
 
-  def total_results
-    query.count
-  end
-
-  def next_page_offset
-    offset.to_i + limit.to_i
-  end
-
-  def has_more?
-    next_page_offset < total_results
-  end
-
-  def limit
-    @limit ||= 12
-  end
-
-  def offset
-    @offset ||= 0
-  end
 end
