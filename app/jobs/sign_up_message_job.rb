8883fe0725d4f296701863eb1be0730cf4a5d100
Keti <ketevanzazarashvili.kz@gmail.com>
2017-05-19 21:43:07 +0400

    Add SignUpMessageJob with temp params

---
+++ app/jobs/sign_up_message_job.rb
@@ -0,0 +1,13 @@
+class SignUpMessageJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(email)
+    client = Twilio::REST::Client.new
+
+    message = client.messages.create(
+      from: ENV['FROM_SMS_PHONE_NUMBER'],
+      to: ENV['TO_SMS_PHONE_NUMBER'],
+      body: I18n.t('message.sign_up', email: email)
+    )
+  end
+end
