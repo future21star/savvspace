9336ed78360eff1e2dde6c8a1619d28958a59a88
Aaron Campos <acampos@dhamira.com>
2017-05-21 16:29:25 -0700

    Ignore bad rupocop link until it's fixed
    https://github.com/bbatsov/rubocop/issues/4189

--- app/models/favorite_item.rb
+++ app/models/favorite_item.rb
@@ -4,9 +4,7 @@ class FavoriteItem < ActiveRecord::Base
 
   validates :favorite, :user, presence: true
 
-  # scope :open_houses, -> { where(favorite_type: "OpenHouse") }
-  scope :open_houses, (lambda { where(favorite_type: 'OpenHouse') })
-
-  # scope :properties, -> { where(favorite_type: "Property") }
-  scope :properties, (lambda { where(favorite_type: 'Property') })
+  # rubocop:disable Lint/AmbiguousBlockAssociation
+  scope :open_houses, -> { where(favorite_type: 'OpenHouse') }
+  scope :properties, -> { where(favorite_type: 'Property') }
 end
