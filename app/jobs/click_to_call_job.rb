517063bff7cd57e29b90396a6ad9ed146e8b2418
Aaron Campos <acampos@dhamira.com>
2017-05-21 17:02:57 -0700

    Rename environment variables
    Also restore previous click to call application SID

--- app/jobs/click_to_call_job.rb
+++ app/jobs/click_to_call_job.rb
@@ -3,7 +3,7 @@ class ClickToCallJob < ActiveJob::Base
 
   def perform(phone_call)
     client = Twilio::REST::Client.new
-    placed_call = client.calls.create(from: '+18052090165',
+    placed_call = client.calls.create(from: ENV.fetch('SAVVSPACE_PHONE_NUMBER'),
                                       to: phone_call.from_phone.number,
                                       applicationSid: ENV.fetch('CLICK_TO_CALL_SID'))
     phone_call.update(sid: placed_call.sid)
--- app/jobs/sign_up_message_job.rb
+++ app/jobs/sign_up_message_job.rb
@@ -5,8 +5,8 @@ class SignUpMessageJob < ActiveJob::Base
     client = Twilio::REST::Client.new
 
     client.messages.create(
-      from: ENV['FROM_SMS_PHONE_NUMBER'],
-      to: ENV['TO_SMS_PHONE_NUMBER'],
+      from: ENV['SAVVSPACE_PHONE_NUMBER'],
+      to: ENV['SIGNUP_SMS_PHONE_NUMBER'],
       body: I18n.t('message.sign_up', email: email)
     )
   end
