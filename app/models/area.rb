6515d359c078db685b1030fa4446c6eaa3e8b350
lionel <lionel02218@yahoo.com>
2017-05-23 23:34:30 +0800

    added validations and dependent options to vendor, area model

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,7 +2,7 @@ class Area < ActiveRecord::Base
   has_one :ambassadorship, dependent: :destroy
   has_one :user, through: :ambassadorship
   has_many :articles, dependent: :nullify
-  has_many :service_areas
+  has_many :service_areas, dependent: :destroy
   has_many :vendors, through: :service_areas
 
   validates :name, presence: true, uniqueness: true
--- app/models/service_area.rb
+++ app/models/service_area.rb
@@ -1,4 +1,6 @@
 class ServiceArea < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :area
+  validates :vendor, presence: true
+  validates :area, presence: true
 end
